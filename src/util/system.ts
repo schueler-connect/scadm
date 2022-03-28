1a8e9a447570a1adb90066058c769c8cb2015e19
Jake <pygamer138@gmail.com>
2022-03-28 17:25:36 +0200

    chore: clean up

--- src/util/system.ts
+++ src/util/system.ts
@@ -1,22 +1,11 @@
 import { release, type, userInfo } from 'os';
-import name from 'linus';
 import tryCatch from './trycatch';
-
-function asyncName() {
-  return new Promise<string>((resolve, reject) => {
-    name((e: any, n: string) => {
-      if (n) resolve(n);
-      reject(e);
-    });
-  });
-}
 
 interface SystemInfo {
   arch: 'x86' | 'arm' | null; // null => unsupported
   bits: 32 | 64 | null; // null => unsupported
   username: string;
   os: string;
-  os_distro: string | null;
   os_release: string;
   os_supported: boolean;
 }
@@ -37,7 +26,6 @@ export async function getSystemInfo(): Promise<SystemInfo> {
     os: type(),
     os_release: release(),
     os_supported: ['Linux', 'Darwin'].includes(type()),
-    os_distro: await tryCatch(async () => await asyncName(), () => null),
   };
 }
 
